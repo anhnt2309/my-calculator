82a04f352155127a3550bd974e9cdc64159a10f2
Tuan_Anh <anhnt.2309@gmail.com>
2020-01-03 23:01:12 +0700

    Format Screen.js code

--- Screen.js
+++ Screen.js
@@ -8,27 +8,16 @@ import * as Constants from "./constants";
 
 class Home extends React.Component {
   render() {
-    let firstRowInputs = [7, 4, 1];
-    let secondRowInputs = [8, 5, 2, 0];
-    let thirdRowInputs = [9, 6, 3, "."];
-    let fourthRowInputs = ["/", "*", "+", "-"];
     return (
       <SafeAreaView style={styles.container}>
         <View style={styles.displayContainer}>
-          <Text
-            adjustsFontSizeToFit={true}
-            numberOfLines={3}
-            ellipsizeMode="tail"
-            style={styles.displayText}
-          >
-            {this.props.displayText}
-          </Text>
+          {Constants.createDisplayText(this.props.displayText)}
         </View>
         <View style={styles.inputContainer}>
           <View style={styles.flex}>
-            <CalculatorButton text={7} outterProps={this.props} />
-            <CalculatorButton text={4} outterProps={this.props} />
-            <CalculatorButton text={1} outterProps={this.props} />
+            {Constants.firstRowInputs.map(value => {
+              return <CalculatorButton text={value} outterProps={this.props} />;
+            })}
             <CalculatorButton
               text={"C"}
               onPress={value => {
@@ -36,39 +25,18 @@ class Home extends React.Component {
               }}
             />
           </View>
+          {Constants.createButton(Constants.secondRowInputs, this)}
+          {Constants.createButton(Constants.thirdRowInputs, this)}
           <View style={styles.flex}>
-            <CalculatorButton text={8} outterProps={this.props} />
-            <CalculatorButton text={5} outterProps={this.props} />
-            <CalculatorButton text={2} outterProps={this.props} />
-            <CalculatorButton text={0} outterProps={this.props} />
-          </View>
-          <View style={styles.flex}>
-            <CalculatorButton text={9} outterProps={this.props} />
-            <CalculatorButton text={6} outterProps={this.props} />
-            <CalculatorButton text={3} outterProps={this.props} />
-            <CalculatorButton text={"."} outterProps={this.props} />
-          </View>
-          <View style={styles.flex}>
-            <CalculatorButton
-              type={Constants.OPERANTS_TYPE}
-              text={"/"}
-              outterProps={this.props}
-            />
-            <CalculatorButton
-              type={Constants.OPERANTS_TYPE}
-              text={"*"}
-              outterProps={this.props}
-            />
-            <CalculatorButton
-              type={Constants.OPERANTS_TYPE}
-              text={"+"}
-              outterProps={this.props}
-            />
-            <CalculatorButton
-              type={Constants.OPERANTS_TYPE}
-              text={"-"}
-              outterProps={this.props}
-            />
+            {Constants.fourthRowInputs.map(value => {
+              return (
+                <CalculatorButton
+                  type={Constants.OPERANTS_TYPE}
+                  text={value}
+                  outterProps={this.props}
+                />
+              );
+            })}
             <CalculatorButton
               type={Constants.OPERANTS_TYPE}
               text={"="}
--- constants.js
+++ constants.js
@@ -1,8 +1,39 @@
-export const NUMBER_TYPE = "NUMBER_TYPE";
-export const OPERANTS_TYPE = "OPERANTS_TYPE";
+import { View, Text } from "react-native";
+import React from "react";
+import CalculatorButton from "./Button";
+import { styles } from "./styles";
 
 export const ACTIONS = {
   INPUT_CHANGE: "INPUT_CHANGE",
   SHOW_RESULT: "SHOW_RESULT",
   CLEAR: "CLEAR"
 };
+export const NUMBER_TYPE = "NUMBER_TYPE";
+export const OPERANTS_TYPE = "OPERANTS_TYPE";
+export const firstRowInputs = [7, 4, 1];
+export const secondRowInputs = [8, 5, 2, 0];
+export const thirdRowInputs = [9, 6, 3, "."];
+export const fourthRowInputs = ["/", "*", "+", "-"];
+
+export function createButton(inputArray, context) {
+  return (
+    <View style={styles.flex}>
+      {inputArray.map(value => {
+        return <CalculatorButton text={value} outterProps={context.props} />;
+      })}
+    </View>
+  );
+}
+
+export function createDisplayText(text) {
+  return (
+    <Text
+      adjustsFontSizeToFit={true}
+      numberOfLines={3}
+      ellipsizeMode="tail"
+      style={styles.displayText}
+    >
+      {text}
+    </Text>
+  );
+}
